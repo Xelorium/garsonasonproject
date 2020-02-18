f43112e520aa2f5293afed8c65499d9fb235fe8a
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2020-02-18 23:29:23 +0300

    Added Project Itself

--- app/src/androidTest/java/com/example/garsonason/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/garsonason/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.garsonason;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
